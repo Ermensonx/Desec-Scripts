4a364328c203c9b4f69289697fdb225819b3228e
Ermenson <99372234+Ermensonx@users.noreply.github.com>
2024-07-19 16:53:56 -0300

    Update sublocationDireto.sh

--- sublocationDireto.sh
+++ sublocationDireto.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
-if ["$1"== ""] 
-then
-echo "modo de uso : sublocation.sh (host)  (wordlist)" 
 
-else 
-for palavra in $(cat $2);  do host $palavra.$1; done | grep -v "NXDOMAIN"
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "modo de uso: sublocation.sh (host) (wordlist)"
+else
+    echo "você gostaria de realizar uma pesquisa direta ou reversa (1 ou 2)?"
+    read resposta
+
+    if [ "$resposta" == "1" ]; then
+        for palavra in $(cat "$2"); do
+            host "$palavra.$1"
+        done | grep -v "NXDOMAIN"
+    else
+        echo "digite o range do bloco de IP que você deseja escanear (ex: 244 266):"
+        read range
+        echo "digite o IP alvo (ex: 123.123.123):"
+        read ip
+        for alvo in $(seq $range); do
+            host -t ptr "$ip.$alvo"
+        done
+    fi
 fi
