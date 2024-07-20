3ff63a6df42efed827cc6ae904145de3b937f294
Ermenson <99372234+Ermensonx@users.noreply.github.com>
2024-07-20 10:16:55 -0300

    Update webrecon.sh

--- webrecon.sh
+++ webrecon.sh
@@ -2,13 +2,33 @@
 
 if [ -z "$1" ] || [ -z "$2" ]; then
     echo "modo de uso: webrecon.sh (http://site) wordlist"
+    exit 1
 else
-    for palavra in $(cat $2); do
-    #conseguimos caso deseje mudar o User-Agent: curl -v -H  "User-Agent: ( user agent)" -s -o /dev/null -w "%{http_code}" $1/$palavra/ 
-    #basta realizar a mudança no codigo
-        resp=$(curl -s -o /dev/null -w "%{http_code}" $1/$palavra/)
-        if [ "$resp" == "200" ]; then
-            echo "Diretório encontrado: $palavra"
-        fi
-    done
+    echo "existe a necessidade de mudar o user agent? (Y/N)"
+    read quest
+    if [ "$quest" == "N" ]; then
+        for palavra in $(cat $2); do
+            resp=$(curl -s -o /dev/null -w "%{http_code}" $1/$palavra/)
+            if [ "$resp" == "200" ]; then
+                echo "Diretório encontrado: $palavra"
+            fi
+            resp2=$(curl -s -o /dev/null -w "%{http_code}" $1/$palavra)
+            if [ "$resp2" == "200" ]; then
+                echo "Arquivo encontrado: $palavra"
+            fi
+        done
+    else
+        echo "qual o user agent"
+        read user
+        for palavra in $(cat $2); do
+            resp=$(curl -s -H "User-Agent: $user" -o /dev/null -w "%{http_code}" $1/$palavra/)
+            if [ "$resp" == "200" ]; then
+                echo "Diretório encontrado: $palavra"
+            fi
+            resp2=$(curl -s -H "User-Agent: $user" -o /dev/null -w "%{http_code}" $1/$palavra)
+            if [ "$resp2" == "200" ]; then
+                echo "Arquivo encontrado: $palavra"
+            fi
+        done
+    fi
 fi
