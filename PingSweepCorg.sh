d4de4fec8f5cb06ad00d2fb661c97ae3965839e5
Ermenson <99372234+Ermensonx@users.noreply.github.com>
2024-07-26 08:05:37 -0300

    Update PingSweepCorg.sh

--- PingSweepCorg.sh
+++ PingSweepCorg.sh
@@ -1,13 +1,11 @@
-#!/bin.bash 
-if ["$1"== ""] 
-then
-echo "Desec -PING Sweep"
+#!/bin/bash
 
-echo "modo de uso: $0 REDE "
-echo "exemplo $0 x.x.x "
-else 
-for host in {1..254};
-do
-ping -c 1 $1.$host | grep "54 bystes" | cut -d ":" f-1 | cut -d " " -f4;
-done
+if [ -z "$1" ]; then
+    echo "Desec - PING Sweep"
+    echo "modo de uso: $0 REDE"
+    echo "exemplo: $0 x.x.x"
+else
+    for host in {1..254}; do
+        ping -c 1 $1.$host | grep "64 bytes" | cut -d " " -f4 | cut -d ":" -f1
+    done
 fi
